6ce720acdce04466b6504fcd574ce1aadafe1a6d
parangat11 <parangataditya001@gmail.com>
2025-01-04 17:14:10 +0530

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+
+export default function Navbar() {
+    return (
+        <>
+            <div>hi</div>
+            <Link to="/">Home</Link>
+            <Link to="/shop">Shop</Link>
+        </>
+    )
+}
---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
+import Shop from './components/Shop.jsx'
+import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "shop",
+    element: <Shop />
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>,
+)
