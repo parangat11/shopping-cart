297090a793eaf458fcc3d8c98af79636c6edcc0a
parangat11 <parangataditya001@gmail.com>
2025-01-04 21:44:12 +0530

    Change directory structure

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import Shop from './components/Shop.jsx'
-import './index.css'
+import './style/index.css'
 
 const router = createBrowserRouter([
   {
